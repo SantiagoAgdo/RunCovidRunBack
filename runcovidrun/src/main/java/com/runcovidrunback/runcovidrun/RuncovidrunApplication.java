11db3d58ddd9b005a428580a0654be2b2fc5d9c9
Santiago Agudelo R <sagudelo@hightech.com.co>
2020-09-07 11:22:08 -0500

    Creacion Proyecto Spring

---
+++ runcovidrun/src/main/java/com/runcovidrunback/runcovidrun/RuncovidrunApplication.java
@@ -0,0 +1,13 @@
+package com.runcovidrunback.runcovidrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuncovidrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuncovidrunApplication.class, args);
+	}
+
+}
---
+++ runcovidrun/src/test/java/com/runcovidrunback/runcovidrun/RuncovidrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runcovidrunback.runcovidrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuncovidrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
